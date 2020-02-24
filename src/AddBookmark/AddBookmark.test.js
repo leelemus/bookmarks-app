887f433247d7ab6493f8b11a940834ae17525bd0
Lee Lemus <leelemus@gmail.com>
2020-02-23 21:00:18 -0600

    using react-router-dom objects for better navigation

--- src/AddBookmark/AddBookmark.test.js
+++ src/AddBookmark/AddBookmark.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router-dom'
 import AddBookmark from './AddBookmark';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AddBookmark />, div);
+  ReactDOM.render(withRouter(<AddBookmark />), div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$tj1Ly52.wfTvUP/M4phm3eW78Y/gB27gyjjF2Prn9l81vEeOzzkoW',
 }
